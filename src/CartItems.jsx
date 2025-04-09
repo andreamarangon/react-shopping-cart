7cb31da54d075aa310ee7354f27edef8c55606de
andreamarangon <marangonandrea84@gmail.com>
2025-04-09 10:56:36 +0200

    first commit

---
+++ src/CartItems.jsx
@@ -0,0 +1,66 @@
+import item1 from './assets/item1.jpg';
+import item2 from './assets/item2.jpg';
+import item3 from './assets/item3.jpg';
+import item4 from './assets/item4.jpg';
+import item5 from './assets/item5.jpg';
+import item6 from './assets/item6.jpg';
+import item7 from './assets/item7.jpg';
+import item8 from './assets/item8.jpg';
+import item9 from './assets/item9.jpg';
+
+export const items = [
+  {
+    id: 1,
+    image: item1,
+    name: "Accessory 1",
+    price: 25.99,
+  },
+  {
+    id: 2,
+    image: item2,
+    name: "Accessory 2",
+    price: 15.99,
+  },
+  {
+    id: 3,
+    image: item3,
+    name: "Accessory 3",
+    price: 35.99,
+  },
+  {
+    id: 4,
+    image: item4,
+    name: "Accessory 4",
+    price: 45.99,
+  },
+  {
+    id: 5,
+    image: item5,
+    name: "Accessory 5",
+    price: 55.99,
+  },
+  {
+    id: 6,
+    image: item6,
+    name: "Accessory 6",
+    price: 65.99,
+  },
+  {
+    id: 7,
+    image: item7,
+    name: "Accessory 7",
+    price: 75.99,
+  },
+  {
+    id: 8,
+    image: item8,
+    name: "Accessory 8",
+    price: 85.99,
+  },
+  {
+    id: 9,
+    image: item9,
+    name: "Accessory 9",
+    price: 95.99,
+  },
+]
